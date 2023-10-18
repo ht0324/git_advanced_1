623bab4e1d25bd93bb03bd934fbdfc7a707dcb62
Hun Tae Kim <huntae324@gmail.com>
2023-10-18 18:20:43 +0900

    Update main.py

--- main.py
+++ main.py
@@ -16,15 +16,25 @@ def even_list(int_list: List[int]) -> List[int]:
 
 # Skeleton code for sum_of_squares_of_even 
 def sum_of_squares_of_even(even_int_list: List[int]) -> int:
-  """ Computes the sum of the squares of all even numbers in a list of integers.
-  Args:
-    even_int_list: A list of even integers.
+    """ Computes the sum of the squares of all even numbers in a list of integers.
 
-  Returns:
-    The sum of the squares of all even numbers in the list. 
-  """ 
-  # TODO: Implement sum_of_squares_of_even 
-  pass
+    Args:
+        even_int_list: A list of even integers.
+
+    Returns:
+        The sum of the squares of all even numbers in the list.
+    """
+    # Initializing a variable to keep track of the sum
+    sum_squares = 0
+    
+    # Iterating through each even integer in the list
+    for number in even_int_list:
+        # Squaring the integer and accumulating the sum
+        sum_squares += number ** 2
+    
+    # Returning the sum of squares of even integers
+    return sum_squares
+
 
 # Main function 
 def main():
